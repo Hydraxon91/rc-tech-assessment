edce3bfc13000ca9c312a44106f8bb06fb416676
Hydraxon91 <benceszabowork@gmail.com>
2024-08-21 11:27:13 +0200

    Task 6

--- diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
@@ -1,39 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { callApi } from "./call-api";
-import Hero from "./interfaces/hero";
+import React from "react";
 import HeroListItem from "./components/HeroListItem";
-import './Styling/HeroList.css'
+import useHeroes from "./hooks/useHeroes";
+import './Styling/HeroList.css';
 
 function HeroesList() {
-  const [heroes, setHeroes] = useState<Hero[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchHeroes = async () => {
-      setLoading(true);
-      setError(null);
-
-      try {
-        const heroList = await callApi<Hero[]>('heroes');
-        setHeroes(heroList);
-      } catch (error) {
-        setError('Failed to fetch heroes.');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchHeroes();
-  }, []);
-
-  const toggleHeroAvailability = (id: number) => {
-    setHeroes((prevHeroes) =>
-      prevHeroes.map((hero) =>
-        hero.id === id ? { ...hero, available: !hero.available } : hero
-      )
-    );
-  };
+  const { heroes, loading, error, toggleHeroAvailability } = useHeroes();
 
   if (loading) {
     return <p>Loading...</p>;
